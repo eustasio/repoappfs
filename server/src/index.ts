f7524ec24835b7241e2cfc7fea07354117a56e33
Florian Sebal <fsebal91@gmail.com>
2025-03-19 18:31:53 +0100

    Intallation des configuration de Express + installation de concurrently pour un lancement du côté client et server

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.status(200).send("<h1>Coucou c'est Florian</h1>");
+});
+
 /**
  * Le server se lance sur le port 8080
  */
