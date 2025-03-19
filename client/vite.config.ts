448e8eaae678ee1fec851a2daa24c20321d74933
Florian Sebal <fsebal91@gmail.com>
2025-03-19 18:10:10 +0100

    Installation de react

---
+++ client/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // Permet d'écouter sur toutes les interfaces réseau
+    port: 4000, // Ou tout autre port que vous préférez
+  },
+})
